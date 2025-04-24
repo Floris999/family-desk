4deadee46e390f1227b60582104b046312b46dfe
Floris999 <florisvandinteren@gmail.com>
2025-04-24 16:21:32 +0200

    Add Wishlist

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,7 +3,7 @@ import HomeRoute from "./Pages/HomeRoute.vue";
 import LoginRoute from "./Pages/LoginRoute.vue";
 import RegisterRoute from "./Pages/RegisterRoute.vue";
 import SettingsRoute from "./Pages/SettingsRoute.vue";
-// import WishListRoute from "./Pages/WishListRoute.vue";
+import WishListRoute from "./Pages/WishListRoute.vue";
 import FormWishlistRoute from "./Pages/FormWishlistRoute.vue";
 import { useAuthStore } from "@stores/authStore";
 
@@ -12,8 +12,8 @@ const routes = [
   { path: "/login", component: LoginRoute },
   { path: "/register", component: RegisterRoute },
   { path: "/settings", component: SettingsRoute },
-  // { path: "/wishlist", component: WishListRoute },
-  { path: "/add-wish", component: FormWishlistRoute},
+  { path: "/wishlist", component: WishListRoute },
+  { path: "/add-wish", component: FormWishlistRoute },
 ];
 
 const router = createRouter({
